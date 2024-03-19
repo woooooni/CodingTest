abf7b6cf5ec91328e70e38c8db1d7b9e69f45d1b
Tiny-Prince <55116786+woooooni@users.noreply.github.com>
2024-03-19 18:42:11 +0900

    [Bronze V] Title: 고양이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.cc
@@ -0,0 +1,13 @@
+
+#include <iostream>
+
+using namespace std;
+int main()
+{
+#pragma region BOJ 10171
+    cout << "\\    /\\" << endl;
+    cout << " )  ( ')" << endl;
+    cout << "(  /  )" << endl;
+    cout << " \\(__)|" << endl;
+#pragma endregion
+}
