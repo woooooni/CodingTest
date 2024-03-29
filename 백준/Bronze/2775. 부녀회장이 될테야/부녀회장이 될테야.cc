77853a8154636efa2333efd56cfbdb67dfac4c41
Tiny-Prince <55116786+woooooni@users.noreply.github.com>
2024-03-29 10:14:15 +0900

    [Bronze I] Title: 부녀회장이 될테야, Time: 92 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int getNum(int x, int y) {
+	if (y == 1) {	
+		return 1;
+	}
+	else if (x == 0) {
+		return y;
+	}
+	else {
+		return (getNum(x - 1, y) + getNum(x, y - 1));
+	}
+}
+
+int main() {
+	int T, k, n;
+	cin >> T;
+
+	for (int i = 0; i < T; i++) {
+		cin >> k >> n;
+		cout << getNum(k, n) << endl;
+	}
+}
