7adb58881db300bc312d53ffc5d444d67f99b35b
TaeWon <55116786+woooooni@users.noreply.github.com>
2024-06-17 14:51:28 +0900

    [Silver II] Title: DFS와 BFS, Time: 8 ms, Memory: 5936 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.cc
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.cc
@@ -1,72 +1,75 @@
 #include <iostream>
-#include <queue>    //BFS에서 활용
-#include <string.h> //memset 쓰려면 필요
+#include <string>
+#include <string.h>
+#include <queue>
+
 using namespace std;
 
-/* 전역으로 정의 - 자동 0 초기화 */
-int arr[1001][1001]; //인접행렬
-int visited[1001];   //방문기록
-int N, M, V;
+int Map[1001][1001] = {};
+bool Visited[1001] = {};
 
-/* DFS - 재귀(시스템 스택 사용) (후입선출 LIFO) */
-void DFS(int V)
+int N, M;
+
+queue<int> q;
+
+void Dfs(int Start)
 {
-    visited[V] = 1;   //시작점 방문기록
-    cout << V << " "; //방문한 노드 출력
-
-    for (int i = 1; i <= N; i++)
+    Visited[Start] = true;
+    cout << Start << " ";
+    
+    for(int i = 1; i <= N; ++i)
     {
-        if (arr[V][i] == 1 && visited[i] == 0)
+        if(1 == Map[Start][i] && false == Visited[i])
         {
-            DFS(i); //스택에 i 넣는 셈
+            Dfs(i);
         }
-        if (i == N)
+        
+        if(i == N)
             return;
     }
 }
 
-/* BFS - 큐 사용 (선입선출 FIFO) */
-void BFS(int V)
+void Bfs(int Start)
 {
-    queue<int> q; //큐 생성
-    q.push(V);    //시작노드 큐에 넣음
-
-    while (!q.empty())
+    q.push(Start);
+    
+    while(!q.empty())
     {
-        int next = q.front(); //큐 맨 앞에 값을 방문
-        visited[next] = 1;    //방문기록
-        cout << next << " ";  //방문한 노드 출력
-        q.pop();              //큐에서 뺌
-
-        //방문했던 노드와 가까운 노드 큐에 넣어줌
+        int Index = q.front();
+        Visited[Index] = true;
+        cout << Index << " ";
+        q.pop();
+        
         for (int i = 1; i <= N; i++)
         {
-            if (arr[next][i] == 1 && visited[i] == 0)
+            if (Map[Index][i] == 1 && Visited[i] == 0)
             {
                 q.push(i);         //큐에 넣어줌
-                visited[i] = 1; // i 점은 미리 방문기록 - 안하면 중복으로 방문할 수도 있다
+                Visited[i] = true; // i 점은 미리 방문기록 - 안하면 중복으로 방문할 수도 있다
             }
         }
+        
     }
-    
 }
 
-int main()
+int main(void)
 {
-    int u, v;
-    cin >> N >> M >> V;
-
-    for (int i = 0; i < M; i++)
+    int Start;
+    cin >> N >> M >> Start;
+    
+    for(int i = 0; i < M; ++i)
     {
-        cin >> u >> v;
-        arr[u][v] = 1;
-        arr[v][u] = 1; //자리바꿔서도 해주는 이유 : 무방향이기 때문
-    }                  //입력 즉시 인접행렬에 넣어줌, 다 돌면 인접행렬 완성
-
-    DFS(V); // DFS 수행
-
-    cout << "\n";                        //개행
-    memset(visited, 0, sizeof(visited)); //방문기록 visited 초기화
-
-    BFS(V); // BFS 수행
+        int From, To;
+        
+        cin >> From >> To;
+        Map[From][To] = 1;
+        Map[To][From] = 1;
+    }
+    
+    Dfs(Start);
+    cout << "\n";
+    
+    memset(Visited, false, sizeof(Visited));
+    
+    Bfs(Start);
 }
