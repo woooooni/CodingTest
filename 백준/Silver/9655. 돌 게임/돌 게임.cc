7ed67953797d9e3812e43221ba5172b139668e9b
TaeWon <55116786+woooooni@users.noreply.github.com>
2024-07-19 11:48:00 +0900

    [Silver V] Title: 돌 게임, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9655. 돌 게임/돌 게임.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+int main(void)
+{
+    int N;
+    cin >> N;
+    
+    if(N % 2 == 0)
+    {
+        cout << "CY";
+    }
+    else
+    {
+        cout << "SK";
+    }
+}
