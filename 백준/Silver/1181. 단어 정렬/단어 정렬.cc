c5ae55dd9622d203e5aa24e4976303bf86abd770
TaeWon <55116786+woooooni@users.noreply.github.com>
2024-08-19 19:25:16 +0900

    [Silver V] Title: 단어 정렬, Time: 80 ms, Memory: 7408 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.cc
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.cc
@@ -1,37 +1,47 @@
 #include <iostream>
+#include <string>
+#include <set>
 #include <algorithm>
+
 using namespace std;
 
-int cmp(string a, string b) {
-	// 1. 길이가 같다면, 사전순으로
-	if (a.length() == b.length()) {
-		return a < b;
-	}
-	// 2. 길이가 다르다면, 짧은 순으로 
-	else {
-		return a.length() < b.length();
-	}
-}
-
-// 범위가 크기때문에 전역변수로 설정
-string word[20000];
-
-int main() {
-	int N;
-
-	cin >> N;
-	for (int i = 0; i < N; i++) {
-		cin >> word[i];
-	}
-
-	sort(word, word + N, cmp);
-
-	for (int i = 0; i < N; i++) {
-		if (word[i] == word[i - 1]) {
-			continue;
-		}
-		cout << word[i] << "\n";
-	}
-
-	return 0;
+int main(void)
+{
+    int N;
+    cin >> N;
+    
+    set<string> WordsSet;
+    
+    for(int i = 0;  i < N; ++i)
+    {
+        string Input;
+        cin >> Input;
+        WordsSet.emplace(Input);
+    }
+    
+    vector<string> Words;
+    
+    for(auto Num :  WordsSet)
+    {
+        Words.push_back(Num);
+    }
+        
+    sort(Words.begin(), Words.end(), [](string Left, string Right)
+    {
+        if(Left.length() == Right.length())
+        {
+            return Left < Right;
+        }
+        else
+        {
+            return Left.length() < Right.length();
+        }
+    });
+    
+    
+    for(int i =0 ; i < Words.size(); ++i)
+    {
+        cout << Words[i] << "\n";
+    }
+    
 }
