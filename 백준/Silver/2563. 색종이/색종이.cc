4f7930760dbf3519defdc68fc7cb821f8ea4ee1a
TaeWon <55116786+woooooni@users.noreply.github.com>
2024-08-23 19:52:17 +0900

    [Silver V] Title: 색종이, Time: 0 ms, Memory: 2072 KB -BaekjoonHub

--- 백준/Silver/2563. 색종이/색종이.cc
+++ 백준/Silver/2563. 색종이/색종이.cc
@@ -1,36 +1,86 @@
 #include <iostream>
 #include <string>
+#include <vector>
+#include <queue>
 
 using namespace std;
 
-int Map[100][100];
+int T;
+
+int Map[101][101];
+bool Visited[101][101];
+
+int DirX[4] = { 0, 0, -1, 1};
+int DirY[4] = { -1, 1, 0, 0};
+
+bool IsIn(int x, int y)
+{
+    return 0 <= x && 0 <= y && x < 100 && y < 100;
+}
+
+int Bfs(int StartX, int StartY)
+{
+    int Check = 1;
+    queue<pair<int, int>> q;
+    Visited[StartY][StartX] = true;
+    q.push(make_pair(StartX, StartY));
+           
+    while(!q.empty())
+    {
+        pair<int, int> Current = q.front();
+        q.pop();
+        
+        for(int i =0 ; i  < 4; ++i)
+        {
+            int NextX = Current.first + DirX[i];
+            int NextY = Current.second + DirY[i];
+            
+            if(IsIn(NextX, NextY))
+            {
+                if(Visited[NextY][NextX] == false && Map[NextY][NextX] == 1)
+                {
+                    Visited[NextY][NextX] = true;
+                    q.push(make_pair(NextX, NextY));
+                    Check++;
+                }
+            }
+        }
+    }
+        
+    return Check;
+}
 
 int main(void)
 {
-    int N;
-    cin >> N;
-    for(int i = 0; i < N; ++i)
+    cin >> T;
+    while(T--)
     {
-        int Col, Row;
-        cin >> Col >> Row;
+        int StartX, StartY;
+        cin >> StartX >> StartY;
+        StartY = 100 - StartY;
         
-        for(int i = Col; i < Col + 10; ++i)
+        for(int i = StartY; i > StartY - 10; --i)
         {
-            for(int j = Row; j < Row + 10; ++j)
+            for(int j = StartX; j < StartX + 10; ++j)
             {
-                Map[j][i] = 1;
+                Map[i][j] = 1;
             }
         }
     }
     
-    int Area = 0;
-    for(int i = 0; i < 100; ++i)
+    int Result = 0;
+    for(int i =0; i <= 100; ++i)
     {
-        for(int j = 0; j < 100; ++j)
+        for(int j = 0 ; j <= 100; ++j)
         {
-             Area += Map[j][i];
+            if(Visited[i][j] == false && Map[i][j] == 1)
+            {
+                Result += Bfs(j, i);
+            }
         }
     }
     
-    cout << Area;
+    cout << Result;
+    
+    
 }
