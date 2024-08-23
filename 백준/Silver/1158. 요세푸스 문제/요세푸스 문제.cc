40a9a2325a62c6b3bfda4e4717a2207663cea194
TaeWon <55116786+woooooni@users.noreply.github.com>
2024-08-23 19:37:08 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 52 ms, Memory: 2040 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.cc
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.cc
@@ -1,34 +1,40 @@
-#include <string>
 #include <iostream>
+#include <string>
+#include <vector>
 #include <queue>
 
 using namespace std;
+int N;
+int K;
+
+int People[5001] = {};
 
 int main(void)
 {
-    int N, K;
     cin >> N >> K;
     
     queue<int> q;
-    for(int i = 1; i<= N; ++i)
+    
+    for(int i = 1; i <= N; ++i)
     {
         q.push(i);
     }
     
     cout << "<";
-    while(!(1 == q.size()))
+    while(q.size() > 1)
     {
-        for(int i = 1; i < K; ++i)
+        for(int i = 0; i < K - 1; ++i)
         {
             int Temp = q.front();
             q.pop();
             q.push(Temp);
         }
         
-        int Res = q.front();
+        int Current = q.front();
         q.pop();
-        
-        cout << Res << ", ";
+        cout << Current << ", ";
     }
     cout << q.front() << ">";
+    
+    
 }
