425fb38c291504d68b4ad80cf7e2dc021f203f23
TaeWon <55116786+woooooni@users.noreply.github.com>
2024-09-04 11:16:39 +0900

    [level 2] Title: 짝지어 제거하기, Time: 5.25 ms, Memory: 6.41 MB -BaekjoonHub

---
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+#include <stack>
+
+using namespace std;
+
+int solution(string s)
+{
+    int answer = 0;
+    if(s.empty() || s.size() % 2 != 0)
+        return answer;
+
+    stack<char> stk;
+    for(size_t nIndex = 0; nIndex < s.size() ; ++nIndex)
+    {
+        if(stk.empty() || stk.top() != s[nIndex])
+            stk.push(s[nIndex]);
+        else
+            stk.pop();
+    }
+
+    if(stk.empty())
+        ++answer;
+
+    return answer;
+}
