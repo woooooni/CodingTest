d5414d253b7f8f6223731ba611f6c73d23ea033c
TaeWon <55116786+woooooni@users.noreply.github.com>
2024-09-05 06:11:40 +0900

    [Gold IV] Title: 부분합, Time: 8 ms, Memory: 2412 KB -BaekjoonHub

---
+++ 백준/Gold/1806. 부분합/부분합.cc
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int arr[100001];
+
+int main(void) {
+    ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+    int N, S;
+    cin >> N >> S;
+    for(int i = 1; i <= N; i++)
+        cin >> arr[i];
+
+    int start = 1, end = 1;
+    int sum = arr[1];
+    int ans = INT_MAX;
+    while(start <= end && end <= N){
+        if(sum >= S) ans = min(ans, end-start+1);
+        if(sum < S){
+            end++;
+            sum += arr[end]; 
+        }else{
+            sum -= arr[start];
+            start++;
+        }
+    }
+    if(ans == INT_MAX) cout << 0 << '\n';
+    else cout << ans << '\n';
+	return 0;
+}
