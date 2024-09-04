f20e02140213b52595ab45aea2b61180f1a2f661
TaeWon <55116786+woooooni@users.noreply.github.com>
2024-09-05 06:38:50 +0900

    [Silver V] Title: 뒤집기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/1439. 뒤집기/뒤집기.cc
+++ 백준/Silver/1439. 뒤집기/뒤집기.cc
@@ -1,29 +1,19 @@
 #include <iostream>
-#include <string>
 
 using namespace std;
 
-int main(void)
-{
-    string Input;
-    cin >> Input;
-    
-    int Zero = 0;
-    int One = 0;
-    
-    for(int i =0 ; i < Input.length(); ++i)
-    {
-        if(Input[i] != Input[i + 1])
-        {
-            if(Input[i] == '0')
-            {
-                Zero++;
-            }
-            else
-            {
-                One++;
-            }
-        }
-    }
-    cout << min(Zero, One);
+string s;
+int result = 0;
+
+int main() {
+    cin >> s;
+
+    for(int i = 0; i < s.length(); i++) 
+        if(s[i] != s[i+1]) 
+            result++;
+
+    if(result == 0)
+        cout << result;
+    else 
+        cout << result / 2;
 }
