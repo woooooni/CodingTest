e9f7f16e211e8730f1b4cbed2464f2a84d26d973
TaeWon <55116786+woooooni@users.noreply.github.com>
2024-09-23 17:51:55 +0900

    [level 1] Title: 약수의 합, Time: 0.02 ms, Memory: 4.18 MB -BaekjoonHub

---
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int n) {
+    int answer = 0;
+
+    for(int i = 2; i<=n; i++)
+    {
+        if(n % i == 0) {
+            answer += i;
+        }
+    }
+
+    if(n != 0) answer++;
+
+    return answer;
+}
