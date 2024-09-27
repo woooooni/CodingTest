02bbef675762ace5b01495dfbf8d085236bf1528
TaeWon <55116786+woooooni@users.noreply.github.com>
2024-09-27 13:32:01 +0900

    [level 3] Title: 단어 변환, Time: 0.02 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.cpp
@@ -0,0 +1,60 @@
+#include <string>
+#include <vector>
+#include <queue>
+#include <algorithm>
+#define MAX 50
+
+using namespace std;
+
+int answer = MAX + 1;
+bool visited[MAX];
+queue<pair<string, int>> q;
+
+bool compareWords(string begin, string word) {
+    int cnt = 0;
+    
+    for (int i=0; i<word.length(); i++) {
+        if (begin[i] == word[i]) {
+            cnt += 1;        
+        }
+    }
+    
+    if (cnt == word.length() - 1) 
+        return true;
+    
+    return false;
+}
+
+void Bfs(string begin, string target, const vector<string>& words) {
+    int len = words.size();
+    q.push({begin, 0});
+    
+    while (!q.empty()) {
+        string word = q.front().first;
+        int level = q.front().second;
+        q.pop();
+        
+        if (word == target) {
+            answer = min(answer, level);
+        }
+        
+        for (int i=0; i<len; i++) {
+            bool isOk = compareWords(word, words[i]);
+            if (visited[i] || !isOk) continue;
+            
+            q.push({words[i], level + 1});
+            visited[i] = true;
+        }
+    }
+}
+
+int solution(string begin, string target, vector<string> words) {
+    auto iter = find(words.begin(), words.end(), target);
+    
+    if (iter == words.end()) 
+        return 0;
+    
+    Bfs(begin, target, words);
+    
+    return answer;
+}
