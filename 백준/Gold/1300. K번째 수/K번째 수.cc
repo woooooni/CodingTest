03b0a13cedb9be73b9a1308c22f0a9b97a0c9e90
TaeWon <55116786+woooooni@users.noreply.github.com>
2024-11-23 11:24:19 +0900

    [Gold I] Title: K번째 수, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/1300. K번째 수/K번째 수.cc
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+int main() {
+	int N, k;
+	cin >> N >> k;
+	int low = 1, high = k;
+	while (low < high) 
+    {
+		int mid = (low + high) / 2, cnt = 0;
+
+		for (int i = 1; i <= N; i++) 
+        {
+			cnt += min(N, mid / i);
+		}
+        //
+		
+		if (cnt < k) 
+            low = mid + 1;
+		else 
+            high = mid;
+	}
+
+	cout << high << '\n';
+}
