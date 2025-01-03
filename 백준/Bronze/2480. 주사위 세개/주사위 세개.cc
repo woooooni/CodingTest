457d49dfdfd4363f90f896928b7720f2ebe679ce
TaeWon <55116786+woooooni@users.noreply.github.com>
2025-01-03 23:58:23 +0900

    [Bronze IV] Title: 주사위 세개, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.cc
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+ 
+int main(int argc, char const *argv[]) {
+ 
+	int a, b, c;
+	cin >> a >> b >> c;
+	// 만약 모든 변수가 다른 경우
+	if (a != b && b != c && a != c) {
+		int max;
+		// 만약 a > b 라면
+		if (a > b) {
+			// c > a > b 라면
+			if (c > a) {
+				max = c;
+			}
+			// a > (b, c)
+			else {
+				max = a;
+			}
+		}
+		// b > a 라면
+		else {
+			// c > b > a 라면
+			if (c > b) {
+				max = c;
+			}
+			// b > (a, c)
+			else {
+				max = b;
+			}
+		}
+		cout << max * 100;
+	}
+	else {
+		if (a == b && a == c) {
+			cout << 10000 + a * 1000;
+		} else {
+			if (a == b || a == c) {
+				cout << 1000 + a * 100;
+			}
+			else {
+				cout << 1000 + b * 100;
+			}
+		}
+	}
+	return 0;
+}
