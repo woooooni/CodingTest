79991cb8187d9d47be814bc5babcc23d7ef89354
TaeWon <55116786+woooooni@users.noreply.github.com>
2025-05-07 22:34:51 +0900

    [Silver IV] Title: 설탕 배달, Time: 0 ms, Memory: 2040 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.cc
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.cc
@@ -1,28 +1,28 @@
 #include <iostream>
-
 using namespace std;
 
-int main(void)
-{
-    int N;
-    int Five,Three;
-    cin >> N;
-    
-    Five = N / 5;
-    N %= 5;
-    
-    while(Five >= 0)
-    {
-        if((N % 3) == 0)
-        {
-            Three = N / 3;
-            N %= 3;
-            break;
-        }
-        Five--;
-        N += 5;
-    }
-    
-    int Answer = N == 0 ? Five + Three : -1;
-    cout << Answer;
+
+int d[5001];
+
+int dp(int x) {
+	if (x < 0) return 99998;
+	if (x == 3 || x == 5) return 1;
+	
+
+	if (d[x] != 0) return d[x];
+	return d[x] = min(dp(x - 3) + 1, dp(x - 5) + 1);
+}
+int main() {
+	cin.tie(0);
+	cout.tie(0);
+	ios::sync_with_stdio(false);
+	d[3] = 1;
+	d[5] = 1;
+	int n;
+	cin >> n;
+	for (int i = 1; i <= n; i++) {
+		dp(n);
+	}
+	if (d[n] >= 6000) cout << "-1";
+	else cout << d[n];
 }
