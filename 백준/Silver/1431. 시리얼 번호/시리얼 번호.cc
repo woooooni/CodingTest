bcd5d315c1a956dd3d84ed0c38b194febaf1b2ba
TaeWon <55116786+woooooni@users.noreply.github.com>
2025-05-18 19:17:16 +0900

    [Silver III] Title: 시리얼 번호, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

--- 백준/Silver/1431. 시리얼 번호/시리얼 번호.cc
+++ 백준/Silver/1431. 시리얼 번호/시리얼 번호.cc
@@ -1,55 +1,55 @@
 #include <iostream>
-#include <algorithm>
+#include <vector>
 #include <string>
+#include <algorithm>
+
 using namespace std;
 
-int cmp(string a, string b) {
-	int asize = a.size();
-	int bsize = b.size();
-	int asum = 0;
-	int bsum = 0;
-
-	// 1. 길이가 다르면, 짧은 순서대로 정렬
-	if (asize != bsize) {
-		return asize < bsize;
-	}
-
-	// 2. 길이가 같다면, 합을 이용해 작은 순서대로 정렬
-	for (int i = 0; i < a.size(); i++) {
-		// 문자열 a에 포함된 숫자들의 합 계산
-		if (a[i] >= '0' && a[i] <= '9') {
-			asum += int(a[i]) - 48;
-		}
-
-		// 문자열 b에 포함된 숫자들의 합 계산
-		if (b[i] >= '0' && b[i] <= '9') {
-			bsum += int(b[i]) - 48;
-		}
-	}
-
-	if (asum != bsum) {
-		return asum < bsum;
-	}
-
-
-	// 3. 1,2 로 비교 불가능하다면, 사전순으로 비교
-	return a < b;
-
+vector<string> strVec;
+int getStrNum(string a)
+{
+    int sum = 0;
+    for(int i = 0; i < a.size(); ++i)
+    {
+        if(a[i] >= '0' && a[i] <= '9')
+            sum += int(a[i]) - 48;
+    }
+    
+    return sum;
 }
-
-int main() {
-	int N;
-	string serial[50];
-
-	cin >> N;
-	for (int i = 0; i < N; i++) {
-		cin >> serial[i];
-	}
-
-	sort(serial, serial + N, cmp);
-	for (int i = 0; i < N; i++) {
-		cout << serial[i] << "\n";
-	}
-	
-	return 0;
+int main(void)
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int N;
+    cin >> N;
+    strVec.reserve(N);
+    for(int i = 0; i < N; ++i)
+    {
+        string input;
+        cin >> input;
+        strVec.push_back(input);
+    }
+    
+    sort(strVec.begin(), strVec.end(), [](string a, string b){
+        if(a.size() != b.size())
+        {
+            return a.size() < b.size();
+        }
+        
+        int sumA = getStrNum(a);
+        int sumB = getStrNum(b);
+        if(sumA != sumB)
+        {
+            return sumA < sumB;
+        }
+        
+        return a < b;
+    });
+    
+    for(int i = 0; i < N; ++i)
+    {
+        cout << strVec[i] << "\n";
+    }
 }
