f07999e99a164e252846be50fb9eb32248105a8c
TaeWon <55116786+woooooni@users.noreply.github.com>
2025-08-21 19:50:30 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+    int a;
+    cin >> a;
+    
+    for(int i = 0; i < a; ++i)
+    {
+        for(int j = 0; j <= i; ++j)
+        {
+            cout << "*";
+        }
+        cout << "\n";
+    }
+}
