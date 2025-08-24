cf2ff90350f53049375af2bb544b5fd8128eb736
TaeWon <55116786+woooooni@users.noreply.github.com>
2025-08-24 22:48:24 +0900

    [Bronze V] Title: 사칙연산, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/10869. 사칙연산/사칙연산.cc
+++ 백준/Bronze/10869. 사칙연산/사칙연산.cc
@@ -1,18 +1,19 @@
-
 #include <iostream>
-#include <string>
 
-#define MAX_ALPHABET 26
 using namespace std;
-int main()
+
+int main(void)
 {
-    int a, b;
-
+    int a,b;
     cin >> a >> b;
-
+    
+    // 첫째 줄에 A+B, 둘째 줄에 A-B, 셋째 줄에 A*B, 넷째 줄에 A/B, 다섯째 줄에 A%B를 출력한다.
+    
     cout << a + b << endl;
     cout << a - b << endl;
     cout << a * b << endl;
     cout << a / b << endl;
     cout << a % b << endl;
+    
+    
 }
